3a09504b85c88bdf7f4a2d590f6f2bcd92eb37a3
Luis Pérez <ljpm9401@gmail.com>
2022-03-28 15:40:10 -0500

    Ajustes actividades didacticas

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -2,6 +2,24 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
+      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
+        .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
+          figure
+            img(src="@/assets/template/emparejamiento.svg", alt="Texto que describa la imagen")
+      .col-12.col-md-8.col-lg-9
+        .titulo-segundo
+          h2 Repasando conceptos claves para la selección de personal
+        p.mb-4 El objetivo de esta actividad es ayudarle a repasar los conocimientos adquiridos en este componente formativo, mediante la organización de palabras que permitan estructurar los conceptos vistos en este componente. Conceptos que se deben conocer al momento de comprender los factores que intervienen en la atención al cliente con base en medios y herramientas tecnológicas. 
+        .tarjeta.tarjeta--azul.p-3
+          .row.justify-content-around.align-items-center            
+            .col-sm.mb-3.mb-sm-0
+              p.fw-bold.mb-0 Rellenar los espacios en blanco 
+            .col-auto
+              a.boton.boton--b(:href="obtenerLink('/actividades/actividad1/')" target="_blank")
+                span Realizar
+                i.fas.fa-puzzle-piece
     
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0

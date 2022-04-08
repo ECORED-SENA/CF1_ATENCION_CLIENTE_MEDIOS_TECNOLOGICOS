5852f4fd1e0ae8406e3c46d092ac014b264004c6
Luis Pérez <ljpm9401@gmail.com>
2022-04-08 13:31:23 -0500

    Ajustes

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -17,7 +17,7 @@
 
     figure.mt-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/asgajqzFAXQ" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/kpV6LQTp5o8" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     p.mt-5(data-aos="fade-up") Por otra parte, encontramos las características de las redes sociales
 
